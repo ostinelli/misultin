e64fb09f72f5fbc2c7aad2582175e6aa2695cc66
Roberto Ostinelli <roberto@ostinelli.net>
2011-09-16 18:41:39 +0200

    set log level default to 'warning' instead of 'error'.

--- include/misultin.hrl
+++ include/misultin.hrl
@@ -40,15 +40,16 @@
 -define(LOG_WARNING(Str, Args), erlang:apply(error_logger, warning_msg, [lists:concat(["	module: ", ?MODULE, "~n	line: ", ?LINE, "~n", Str, "~n"]), Args])).
 -define(LOG_ERROR(Str, Args), erlang:apply(error_logger, error_msg, [lists:concat(["	module: ", ?MODULE, "~n	line: ", ?LINE, "~n", Str, "~n"]), Args])).
 -else.
--ifdef(log_warning).
--define(LOG_DEBUG(Str, Args), ok).
--define(LOG_INFO(Str, Args), ok).
--define(LOG_WARNING(Str, Args), erlang:apply(error_logger, warning_msg, [lists:concat(["	module: ", ?MODULE, "~n	line: ", ?LINE, "~n", Str, "~n"]), Args])).
--define(LOG_ERROR(Str, Args), erlang:apply(error_logger, error_msg, [lists:concat(["	module: ", ?MODULE, "~n	line: ", ?LINE, "~n", Str, "~n"]), Args])).
--else.
+-ifdef(log_error).
 -define(LOG_DEBUG(Str, Args), ok).
 -define(LOG_INFO(Str, Args), ok).
 -define(LOG_WARNING(Str, Args), ok).
+-define(LOG_ERROR(Str, Args), erlang:apply(error_logger, error_msg, [lists:concat(["	module: ", ?MODULE, "~n	line: ", ?LINE, "~n", Str, "~n"]), Args])).
+-else.
+% default to warning level
+-define(LOG_DEBUG(Str, Args), ok).
+-define(LOG_INFO(Str, Args), ok).
+-define(LOG_WARNING(Str, Args), erlang:apply(error_logger, warning_msg, [lists:concat(["	module: ", ?MODULE, "~n	line: ", ?LINE, "~n", Str, "~n"]), Args])).
 -define(LOG_ERROR(Str, Args), erlang:apply(error_logger, error_msg, [lists:concat(["	module: ", ?MODULE, "~n	line: ", ?LINE, "~n", Str, "~n"]), Args])).
 -endif.
 -endif.
